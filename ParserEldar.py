28e841bb225ecfb3f113dfe17525c137a569f737
kirik <kirik550088@gmail.com>
2021-10-18 21:33:02 +0300

    'Good'

--- ParserEldar.py
+++ ParserEldar.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://kinoklub-eldar.ru/kinoafisha'
+url = 'https://afisha.yandex.ru/moscow/cinema/places/kinoklub-eldar?source=search-page&place-schedule-preset=today'
 user_agent = {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'
     }
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-name_movie = soup.find_all('p', class_= 'kb_widget_theatre_film_info_name')
-print(type(name_movie))
-for i in range(0, len(name_movie)):
-    # print(name_movie[i].text)
-    print(type(name_movie[i]))
+data = ''
+info = []
+name_movie = soup.find_all('div', class_= 'schedule-event i-page__item i-page__item_loader_yes i-bem')
+for n in enumerate(name_movie, start=1):
+    bl = n.find('h3', class_= 'schedule-event__title')
+    print(bl.text)
+
